a30e1acef3a42cc5435c8889fed7c2a3516a0ce3
GXQ <18811446908@163.com>
2017-09-14 11:00:19 +0800

    TcpServer
    1、初步引入TcpServer

--- pytdx/TcpServer/Server.py
+++ pytdx/TcpServer/Server.py
@@ -1,55 +1,41 @@
+# -*- coding:utf-8 -*-
 
-import optparse
-import os
-import socket
-import time
-from threading import Thread
-from io import StringIO
+from socketserver  import TCPServer, BaseRequestHandler
+import traceback
 
-txt = '''1111
-2222
-3333
-4444
-'''
 
-# 服务端程序处理过程
-def server(listen_socket):
-	while True:
-		buf = StringIO(txt)
-		sock, addr = listen_socket.accept()
-		print('Somebody at %s wants poetry!' %(addr,))
-		while True:
-			try:
-				line = buf.readline().strip()
-				if not line:
-					sock.close()
-					break
-				sock.sendall(line.encode('utf8')) # this is a blocking call
-				print('send bytes to client: %s' %line)
-			except socket.error:
-				sock.close()
-				break
-			time.sleep(0.5) # server每发送一个单词后等待一会
-		sock.close()
-		print('\n')
+class MyBaseRequestHandlerr(BaseRequestHandler):
+    """
+    #从BaseRequestHandler继承，并重写handle方法
+    """
 
-# 同时开启三个服务端线程，分别在三个端口监听
-# 服务端程序为阻塞方式，只能一次服务于一个客户端
-def main():
-	ports = [10010, 10011, 10012]
-	for port in ports:
-		listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		addres = ('127.0.0.1', port)
-		listen_socket.bind(addres)
-		listen_socket.listen(5)
-		print("start listen at: %d" %port)
-		worker = Thread(target = server, args = [listen_socket])
-		worker.setDaemon(True)
-		worker.start()
+    def handle(self):
+        # 循环监听（读取）来自客户端的数据
+        while True:
+            # 当客户端主动断开连接时，self.recv(1024)会抛出异常
+            try:
+                # 一次读取1024字节,并去除两端的空白字符(包括空格,TAB,\r,\n)
+                data = self.request.recv(1024).strip()
 
-if __name__ == '__main__':
-	main()
-	while True:
-		time.sleep(0.1) # 如果不sleep的话, CPU会被Python完全占用了
+                # self.client_address是客户端的连接(host, port)的元组
+                print
+                "receive from (%r):%r" % (self.client_address, data)
 
+                # 转换成大写后写回(发生到)客户端
+                self.request.sendall(data.upper())
+            except:
+                traceback.print_exc()
+                break
+
+
+if __name__ == "__main__":
+    # telnet 127.0.0.1 9999
+    host = ""  # 主机名，可以是ip,像localhost的主机名,或""
+    port = 9999  # 端口
+    addr = (host, port)
+
+    # 购置TCPServer对象，
+    server = TCPServer(addr, MyBaseRequestHandlerr)
+
+    # 启动服务监听
+    server.serve_forever()
